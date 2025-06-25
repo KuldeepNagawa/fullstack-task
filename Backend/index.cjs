366d5bd40aee4d00a1cf172504b572c30c7a3c14
KuldeepNagawa <kuldeepnagawa7@gmail.com>
2025-06-25 17:29:08 +0530

    change1

--- Backend/index.cjs
+++ Backend/index.cjs
@@ -1,8 +1,7 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
-// other imports
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 // const dotenv = require('dotenv');
 // const cors = require('cors');
